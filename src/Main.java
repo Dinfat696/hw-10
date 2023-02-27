a1cd2d2295634ff38d07315ac9f1014d7bc88093
dinarfathrahmanov <dinfat@mail.ru>
2023-02-27 23:06:05 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+           /*task2();
+            task3();
+            task4();*/
+
+    }
+
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich ";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника — "+ fullName);
     }
 }
